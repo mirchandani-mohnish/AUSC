838cf8d581a2317248eb74887542ae85b5527227
Mohnish <mirchandani.mohnish03@gmail.com>
2021-07-10 15:07:51 +0530

    some mongodb connections established

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var app = express();
 const mongoose = require('mongoose');
 const path = require('path');
+var bodyParser = require('body-parser');
+var user = require('./models/user');
+
+// use bodyparser 
+app.use(bodyParser.urlencoded());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+
 
 
 app.set('view engine','ejs');
@@ -30,14 +42,67 @@ app.get('/login',function(req,res){
     res.render('loginpg/login');
 });
 
+
+// site register page 
+app.get('/register', function(req,res){
+    res.render('loginpg/signup');
+});
+
+
+mongoose.connect('mongodb://localhost:27017/',{useNewUrlParser: true , useUnifiedTopology: true });
+const db = mongoose.connection;
+
+app.post('/register',function(req,res){
+    try{
+        let tempuser = new user({
+            userName: req.body.uname,
+            email: req.body.email,
+            password: req.body.password
+        });
+        
+
+        tempuser.save(function(err,res){
+            if(err){
+                console.log("error");
+            }else{
+                console.log("document saved");
+            }
+        });
+        
+
+    }
+    catch(error){
+
+        res.send("invalid ");
+    }
+})
+
+
+// mongoose database connection
+/*
+mongoose.connect('mongodb://localhost:27017/',{useNewUrlParser: true});
+const db = mongoose.connection;
+const query = user.find();
 app.post('/login',function(req,res){
+    try{
+
     
         const email = req.body.email;
         const pass = req.body.password;
+        
 
-        console.log("email and password submitted ");
+        
+    }
+    catch(error){
+
+    
+        
+        res.send("invalid ");
+    }
     
 })
+*/
+
 // /------
 
 
---
+++ models/user.js
@@ -0,0 +1,7 @@
+var express = require('express');
+const mongoose = require('mongoose');
+
+
+const Schema = mongoose.Schema;
+
+
