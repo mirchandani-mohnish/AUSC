1de5b80b1ba919516bf5068efd99e0ad36e8dbde
Mohnish <mirchandani.mohnish03@gmail.com>
2021-07-22 16:39:06 +0530

    user panel made -- partially working

--- routes/adminpg.js
+++ routes/adminpg.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var course = require('../models/course');
+var jwt = require('jsonwebtoken');
+var user = require('../models/user');
 
 // mongodb connection
 const mongoose = require('mongoose');
@@ -46,6 +48,54 @@ router.post('/add',async function(req,res){
     
 });
 
+router.get('/userpanel', async function(req,res){
+    let accessToken = req.cookies.mcook
+    if (!accessToken){
+        return res.status(403).send()
+    }
+    let payload
+    try{
+        
+        payload = jwt.verify(accessToken, "bcozimbatman");
+        
+        var userlist;
+        await user.find({}).then(users => userlist = users).catch(error => console.log(error));
+        res.render('compAdmin/userpanel',{userlist: userlist});
+        
+    }
+    catch(e){
+        //if an error occured return request unauthorized error
+        
+        console.log(e);
+        res.send("please sign in again");
+
+    
+        
+    }
+  
+    
+    
+    //res.render('courseAdmin/userpanel',{allusers: allusers})
+});
+
+
+router.get('/userpanel/addcourse/:edituser', async function(req,res){
+    var edituser = req.params.edituser;
+    var curruser = await user.findOne({userName: edituser.username});
+    if(typeof(curruser.courses) === 'undefined'){
+        var temparr = [edituser.code];
+        curruser.courses = temparr;
+    }else{
+        curruser.courses.push(edituser.code);
+    }
+
+    curruser.save().then(res.redirect('/admin/userpanel')).catch(e => console.log(e));
+
+
+});
+
+// a userpanel function which categorizes students and also displays all the courses that they
+//have chosen -- it should allow an admin to allot courses to students when required
 
 
 module.exports = router;
